d74f741d862544a6f6e0904329f8d03b463470b8
hongkikii <hyangmeeh@icloud.com>
2024-01-16 00:19:13 +0900

    [자바 알고리즘 문제풀이 입문] 01-02 아스키 코드 적용

--- src/main/java/com/example/codingtest/inflearn1/Q0102.java
+++ src/main/java/com/example/codingtest/inflearn1/Q0102.java
@@ -8,11 +8,17 @@ public class Q0102 {
         String str = sc.nextLine();
         String result = "";
         for (char c : str.toCharArray()) {
-            if (Character.isLowerCase(c)) {
-                result += Character.toUpperCase(c);
+//            if (Character.isLowerCase(c)) {
+//                result += Character.toUpperCase(c);
+//            }
+//            if (Character.isUpperCase(c)) {
+//                result += Character.toLowerCase(c);
+//            }
+            if (c >= 97 && c <= 122) {
+                result += (char)(c - 32);
             }
-            if (Character.isUpperCase(c)) {
-                result += Character.toLowerCase(c);
+            if (c >= 65 && c <= 90) {
+                result += (char)(c + 32);
             }
         }
         System.out.print(result);
