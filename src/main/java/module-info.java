d9ca745eea45064b76c8954a0280361257ed9f18
hongkikii <hyangmeeh@icloud.com>
2024-01-16 00:31:33 +0900

    .gitignore 설정 추가

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.codingtest {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.example.codingtest to javafx.fxml;
-    exports com.example.codingtest;
-    exports com.example.codingtest.inflearn1;
-    opens com.example.codingtest.inflearn1 to javafx.fxml;
-}
