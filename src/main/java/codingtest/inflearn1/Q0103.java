35f4a670dba45d7ea8ffaf12240bd4e9b6454ef8
hongkikii <hyangmeeh@icloud.com>
2024-01-16 18:36:55 +0900

    [자바 알고리즘 문제풀이 입문] 01-03 정답 코드

--- src/main/java/codingtest/inflearn1/Q0103.java
+++ src/main/java/codingtest/inflearn1/Q0103.java
@@ -1,7 +1,20 @@
 package codingtest.inflearn1;
 
+import java.util.Scanner;
+
 public class Q0103 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        String[] split = line.split(" ");
+        String result = "";
+        int maxCount = 0;
+        for (int i = 0; i < split.length; i++) {
+            if (split[i].length() > maxCount) {
+                result = split[i];
+                maxCount = split[i].length();
+            }
+        }
+        System.out.println(result);
     }
 }
