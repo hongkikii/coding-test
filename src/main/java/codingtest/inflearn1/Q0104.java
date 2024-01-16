14f082b351e947aef490e47dd4f50be539f5a1cb
hongkikii <hyangmeeh@icloud.com>
2024-01-16 19:15:01 +0900

    [자바 알고리즘 문제풀이 입문] 01-04 정답 코드

--- src/main/java/codingtest/inflearn1/Q0104.java
+++ src/main/java/codingtest/inflearn1/Q0104.java
@@ -1,2 +1,16 @@
-package codingtest.inflearn1;public class Q0104 {
+package codingtest.inflearn1;
+
+import java.util.Scanner;
+
+public class Q0104 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int loop = sc.nextInt(); // 공백이나 개행 전까지 읽음, 개행 문자를 읽지 않음 = 남아 있음
+        sc.nextLine(); // 개행 문자 소비
+        for (int i = 0; i < loop; i++) {
+            String str = sc.nextLine();
+            StringBuilder stringBuilder = new StringBuilder(str).reverse();
+            System.out.println(stringBuilder); // 객체의 toString() 메소드 자동으로 호출
+        }
+    }
 }
