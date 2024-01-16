b9836a34778d415b7d91e3d35406242ca4a204f7
hongkikii <hyangmeeh@icloud.com>
2024-01-16 18:41:39 +0900

    [자바 알고리즘 문제풀이 입문] 01-03 Arrays.stream 적용

--- src/main/java/codingtest/inflearn1/Q0103.java
+++ src/main/java/codingtest/inflearn1/Q0103.java
@@ -1,5 +1,7 @@
 package codingtest.inflearn1;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Q0103 {
@@ -7,14 +9,9 @@ public class Q0103 {
         Scanner sc = new Scanner(System.in);
         String line = sc.nextLine();
         String[] split = line.split(" ");
-        String result = "";
-        int maxCount = 0;
-        for (int i = 0; i < split.length; i++) {
-            if (split[i].length() > maxCount) {
-                result = split[i];
-                maxCount = split[i].length();
-            }
-        }
+        String result = Arrays.stream(split)
+                .max(Comparator.comparingInt(String::length))
+                .orElse("");
         System.out.println(result);
     }
 }
