82ebfe6e84b77f0b5e0656d3929111d004684176
hongkikii <hyangmeeh@icloud.com>
2024-01-18 03:03:27 +0900

    [자바 알고리즘 문제풀이 입문] 01-09 정답 코드

--- src/main/java/codingtest/inflearn1/Q0109.java
+++ src/main/java/codingtest/inflearn1/Q0109.java
@@ -6,6 +6,9 @@ public class Q0109 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String line = sc.nextLine().replaceAll("[^0-9]", "");
+        while (line.charAt(0) == '0') {
+            line = line.substring(1);
+        }
         System.out.println(line);
     }
 }
