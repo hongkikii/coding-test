18ec0e57338d0750dca045d2a9f2b381fb1f7ea6
hongkikii <hyangmeeh@icloud.com>
2024-01-18 02:19:35 +0900

    [자바 알고리즘 문제풀이 입문] 01-07 for문+/2 적용

--- src/main/java/codingtest/inflearn1/Q0107.java
+++ src/main/java/codingtest/inflearn1/Q0107.java
@@ -6,15 +6,12 @@ public class Q0107 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String line = sc.nextLine().toLowerCase();
-        int left = 0;
-        int right = line.length() - 1;
-        while (left < right) {
-            if (line.charAt(left) != line.charAt(right)) {
+        int length = line.length();
+        for (int i = 0; i < length / 2; i++) {
+            if (line.charAt(i) != line.charAt(length - 1 - i)) {
                 System.out.println("NO");
                 return;
             }
-            left++;
-            right--;
         }
         System.out.println("YES");
     }
