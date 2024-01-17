5091a564725f72310745b48c2f0a2f4ce76970e0
hongkikii <hyangmeeh@icloud.com>
2024-01-18 03:02:24 +0900

    [자바 알고리즘 문제풀이 입문] 01-09 오답 코드

---
+++ src/main/java/codingtest/inflearn1/Q0109.java
@@ -0,0 +1,11 @@
+package codingtest.inflearn1;
+
+import java.util.Scanner;
+
+public class Q0109 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine().replaceAll("[^0-9]", "");
+        System.out.println(line);
+    }
+}
