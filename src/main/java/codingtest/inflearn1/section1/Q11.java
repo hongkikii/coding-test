6952b629e48d89bd1f9bb81aedc2ba504eb9829c
hongkikii <hyangmeeh@icloud.com>
2024-01-24 03:34:41 +0900

    [자바 알고리즘 문제풀이 입문] 01-11 i == i+1 비교 적용

--- src/main/java/codingtest/inflearn1/section1/Q11.java
+++ src/main/java/codingtest/inflearn1/section1/Q11.java
@@ -5,27 +5,20 @@ import java.util.Scanner;
 public class Q11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String line = sc.nextLine();
+        String line = sc.nextLine() + " ";
         String result = "";
+        int count = 1;
 
-        for (int i = 0; i < line.length(); i++) {
-            char alphabet = line.charAt(i);
-            if (i == 0) {
-                result += alphabet;
-                continue;
+        for (int i = 0; i < line.length() - 1; i++) {
+            if (line.charAt(i) == line.charAt(i + 1)) {
+                count++;
             }
-            if (alphabet == line.charAt(i - 1)) {
-                char lastCharacter = result.charAt(result.length() - 1);
-                if (lastCharacter >= 48 && lastCharacter <= 57) {
-                    int newCount = Integer.parseInt(String.valueOf(lastCharacter)) + 1;
-                    result = result.substring(0, result.length() - 1) + newCount;
+            else {
+                result += line.charAt(i);
+                if (count > 1) {
+                    result += count;
+                    count = 1;
                 }
-                else {
-                    result += 2;
-                }
-            }
-            if (alphabet != line.charAt(i - 1)) {
-                result += alphabet;
             }
         }
         System.out.println(result);
