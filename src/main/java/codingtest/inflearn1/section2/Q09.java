241356471e51bbcdc4c50c6c41b83db7b9703ed3
hongkikii <hyangmeeh@icloud.com>
2024-01-28 19:21:21 +0900

    [자바 알고리즘 문제풀이 입문] 02-09 정답 코드

---
+++ src/main/java/codingtest/inflearn1/section2/Q09.java
@@ -0,0 +1,48 @@
+package main.java.codingtest.inflearn1.section2;
+
+import java.util.Scanner;
+
+public class Q09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[][] A = new int[N + 1][N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                A[i][j] = sc.nextInt();
+            }
+        }
+
+        int max = 0;
+        for (int i = 1; i <= N; i++) {
+            int row = 0;
+            for (int j = 1; j <= N; j++) {
+                row += A[i][j];
+            }
+            max = Math.max(max, row);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            int column = 0;
+            for (int j = 1; j <= N; j++) {
+                column += A[j][i];
+            }
+            max = Math.max(max, column);
+        }
+
+        int leftDiagonal = 0;
+        for (int i = 1; i <= N; i++) {
+            leftDiagonal += A[i][i];
+        }
+        max = Math.max(max, leftDiagonal);
+
+        int rightDiagonal = 0;
+        for (int i = 1; i <= N; i++) {
+            rightDiagonal += A[i][N - i + 1];
+        }
+        max = Math.max(max, rightDiagonal);
+
+        System.out.println(max);
+    }
+}
