c3b00cebf6835fb51c79345a691c581c51125cfa
hongkikii <hyangmeeh@icloud.com>
2024-02-02 00:04:37 +0900

    [자바 알고리즘 문제풀이 입문] 03-03 정답 코드

---
+++ src/main/java/codingtest/inflearn1/section3/Q03.java
@@ -0,0 +1,31 @@
+package main.java.codingtest.inflearn1.section3;
+
+import java.util.Scanner;
+
+public class Q03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int[] A = new int[N];
+        int max = 0;
+        int sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i <= K - 1; i++) {
+            sum += A[i];
+        }
+        max = sum;
+
+        for (int i = K; i < N; i++) {
+            sum -= A[i - K];
+            sum += A[i];
+            max = Math.max(max, sum);
+        }
+
+        System.out.println(max);
+    }
+}
