6031368f35b9a2aa2868c427718b0d377ac2489d
hongkikii <hyangmeeh@icloud.com>
2024-02-03 05:01:08 +0900

    [자바 알고리즘 문제풀이 입문] 04-01 getOrDefault() 메소드 적용

--- src/main/java/codingtest/inflearn1/section4/Q01.java
+++ src/main/java/codingtest/inflearn1/section4/Q01.java
@@ -2,35 +2,27 @@ package main.java.codingtest.inflearn1.section4;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Scanner;
 
 public class Q01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
         Map<Character, Integer> votes = new HashMap<>();
-        for (int i = 0; i < 26; i++) {
-            int key = 'A' + i;
-            votes.put((char) key, 0);
+        sc.nextInt();
+        char[] chars = sc.next().toCharArray();
+
+        for (char c : chars) {
+            votes.put(c, votes.getOrDefault(c, 0) + 1);
         }
 
-        String line = sc.next();
-        char[] chars = line.toCharArray();
-
-        for (int i = 0; i < N; i++) {
-            char c = chars[i];
-            votes.entrySet().stream()
-                    .filter(entry -> entry.getKey() == c)
-                    .forEach(entry -> votes.put(entry.getKey(), entry.getValue() + 1));
+        int max = 0;
+        char answer = ' ';
+        for (char c : votes.keySet()) {
+            if (votes.get(c) > max) {
+                max = votes.get(c);
+                answer = c;
+            }
         }
-
-        Character result = votes.entrySet().stream()
-                .max(Entry.comparingByValue())
-                .map(Entry::getKey)
-                .orElse(null);
-
-        System.out.println(result);
-
+        System.out.println(answer);
     }
 }
