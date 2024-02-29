c565cb6d405f955e7e8f76162bfa15a702f0702e
hongkikii <hyangmeeh@icloud.com>
2024-03-01 01:47:16 +0900

    [자바 알고리즘 문제풀이 입문] 05-07 오답 코드

--- src/main/java/codingtest/inflearn1/section5/Q07.java
+++ src/main/java/codingtest/inflearn1/section5/Q07.java
@@ -1,2 +1,34 @@
-package main.java.codingtest.inflearn1.section5;public class Q07 {
+package main.java.codingtest.inflearn1.section5;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Q07 {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        char[] sequence = sc.nextLine().toCharArray();
+        char[] plan = sc.nextLine().toCharArray();
+        Queue<Character> queue = new LinkedList<>();
+        for(char c : sequence) {
+            queue.offer(c);
+        }
+
+        for(int i=0; i<plan.length; i++) {
+            if(plan[i] == queue.peek()) {
+                queue.poll();
+            }
+        }
+
+        if(queue.size() == 0) {
+            System.out.println("YES");
+        }
+        else {
+            System.out.println("NO");
+        }
+
+    }
+
 }
