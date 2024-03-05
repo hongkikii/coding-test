0c394eea00a039e9aa60e91190a020e0222c5e2c
hongkikii <hyangmeeh@icloud.com>
2024-03-06 01:41:14 +0900

    [자바 알고리즘 문제풀이 입문] 06-04 for문 개선

--- src/main/java/codingtest/inflearn1/section6/Q04.java
+++ src/main/java/codingtest/inflearn1/section6/Q04.java
@@ -8,25 +8,31 @@ public class Q04 {
         int S = sc.nextInt();
         int N = sc.nextInt();
         int[] cache = new int[S];
+        int[] A = new int[N];
 
-        for(int i=0; i<N; i++) {
-            int value = sc.nextInt();
-            for(int j = 0; j<S; j++) {
-                if(value == cache[j]) {
-                    for(int k=j; k>0; k--) {
-                        cache[k] = cache[k-1];
-                    }
-                    cache[0] = value;
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+
+        for (int v : A) {
+            int idx = -1;
+            for (int j = 0; j < S; j++) {
+                if (cache[j] == v) {
+                    idx = j;
                     break;
                 }
-                else {
-                    if(j == S-1) {
-                        for(int l=j; l>0; l--) {
-                            cache[l] = cache[l-1];
-                        }
-                        cache[0] = value;
-                    }
+            }
+            if (idx == -1) {
+                for (int k = S - 1; k > 0; k--) {
+                    cache[k] = cache[k - 1];
                 }
+                cache[0] = v;
+            }
+            else {
+                for (int k = idx; k > 0; k--) {
+                    cache[k] = cache[k - 1];
+                }
+                cache[0] = v;
             }
         }
 
