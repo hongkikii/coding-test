843b026473486b62724b64c005378fc9fe5e7226
hongkikii <hyangmeeh@icloud.com>
2024-03-06 20:55:52 +0900

    [자바 알고리즘 문제풀이 입문] 06-06 정렬 적용

--- src/main/java/codingtest/inflearn1/section6/Q06.java
+++ src/main/java/codingtest/inflearn1/section6/Q06.java
@@ -1,5 +1,6 @@
 package main.java.codingtest.inflearn1.section6;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Q06 {
@@ -7,21 +8,28 @@ public class Q06 {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int[] A = new int[N+1];
+        int[] B = new int[N+1];
         int tall = -1;
         int small = -1;
 
         for(int i=1; i<=N; i++) {
             A[i] = sc.nextInt();
+            B[i] = A[i];
         }
 
-        for(int i=2; i<N; i++) {
-            if(A[i] < A[i-1]) {
-                small = i;
-            }
-            if(tall == -1 && A[i] >= A[i+1]) {
-                tall = i;
+        Arrays.sort(B);
+
+        for(int i=1; i<=N; i++) {
+            if(A[i] != B[i]) {
+                if(B[i] > A[i]) {
+                    tall = i;
+                }
+                if(B[i] < A[i]) {
+                    small = i;
+                }
             }
         }
-        System.out.println(tall  + " " + small);
+        System.out.println(small + " " + tall);
     }
+
 }
