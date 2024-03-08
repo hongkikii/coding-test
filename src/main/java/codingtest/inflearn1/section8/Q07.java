ce8f5d7c59ae5404b9e6ff93caf01bfd1a6f460b
hongkikii <hyangmeeh@icloud.com>
2024-03-08 19:45:20 +0900

    [자바 알고리즘 문제풀이 입문] 08-07 오답 코드

---
+++ src/main/java/codingtest/inflearn1/section8/Q07.java
@@ -0,0 +1,25 @@
+package main.java.codingtest.inflearn1.section8;
+
+import java.util.Scanner;
+
+public class Q07 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int r = sc.nextInt();
+
+        int answer = DFS(n, r);
+
+        System.out.println(answer);
+    }
+
+    public static int DFS(int n, int r) {
+        if(n == 1) return 1;
+        if(r == 1) return n;
+        else {
+            return DFS(n-1, r-1) + DFS(n-1, r);
+        }
+    }
+
+}
