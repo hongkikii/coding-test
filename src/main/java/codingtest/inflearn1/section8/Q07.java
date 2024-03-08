411c0734c6642f5f17c914d88edd2ef27e8500bf
hongkikii <hyangmeeh@icloud.com>
2024-03-08 19:52:27 +0900

    [자바 알고리즘 문제풀이 입문] 08-07 +메모이제이션 적용 정답 코드

--- src/main/java/codingtest/inflearn1/section8/Q07.java
+++ src/main/java/codingtest/inflearn1/section8/Q07.java
@@ -3,11 +3,12 @@ package main.java.codingtest.inflearn1.section8;
 import java.util.Scanner;
 
 public class Q07 {
+    static int[][] memo;
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int r = sc.nextInt();
+        memo = new int[n + 1][r + 1];
 
         int answer = DFS(n, r);
 
@@ -17,7 +18,8 @@ public class Q07 {
     public static int DFS(int n, int r) {
         if(n == r || r == 0) return 1;
         else {
-            return DFS(n-1, r-1) + DFS(n-1, r);
+            if(memo[n][r] != 0) return memo[n][r];
+            return memo[n][r] = DFS(n-1, r-1) + DFS(n-1, r);
         }
     }
 }
