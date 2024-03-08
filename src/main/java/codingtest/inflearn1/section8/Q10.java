07f0b6c581cee4613e01f44d836d953e6005c48c
hongkikii <hyangmeeh@icloud.com>
2024-03-08 23:51:55 +0900

    [자바 알고리즘 문제풀이 입문] 08-10 개선된 DFS 적용

--- src/main/java/codingtest/inflearn1/section8/Q10.java
+++ src/main/java/codingtest/inflearn1/section8/Q10.java
@@ -5,11 +5,12 @@ import java.util.Scanner;
 public class Q10 {
     static int count;
     static int[][] path;
-    static int[][] check;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, -1, 0, 1};
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         path = new int[8][8];
-        check = new int[10][10];
 
         for(int i=1; i<=7; i++) {
             for(int j=1; j<=7; j++) {
@@ -17,22 +18,22 @@ public class Q10 {
             }
         }
 
+        path[1][1] = 1;
         DFS(1, 1);
         System.out.print(count);
     }
 
     public static void DFS(int a, int b) {
-        if(a > 7 || b > 7 || a <= 0 || b <= 0) return;
-        if(path[a][b] == 1) return;
         if(a == 7 && b == 7) count++;
         else {
-            if (check[a][b] == 0) {
-                check[a][b] = 1;
-                DFS(a-1, b);
-                DFS(a, b - 1);
-                DFS(a + 1, b);
-                DFS(a, b + 1);
-                check[a][b] = 0;
+            for (int i = 0; i < 4; i++) {
+                int x = a + dx[i];
+                int y = b + dy[i];
+                if (x >= 1 && x <= 7 && y >= 1 && y <= 7 && path[x][y] == 0) {
+                    path[x][y] = 1;
+                    DFS(x, y);
+                    path[x][y] = 0;
+                }
             }
         }
     }
