d8a4e64a687deb6cedbfe644c20d5a381b6f6a1b
hongkikii <hyangmeeh@icloud.com>
2024-03-08 19:33:17 +0900

    [자바 알고리즘 문제풀이 입문] 08-06 오답 코드

---
+++ src/main/java/codingtest/inflearn1/section8/Q06.java
@@ -0,0 +1,42 @@
+package main.java.codingtest.inflearn1.section8;
+
+import java.util.Scanner;
+
+public class Q06 {
+    static int N;
+    static int M;
+    static int[] A;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        A = new int[N];
+        int[] answer = new int[M];
+
+        for(int i=0; i<N; i++) {
+            A[i] = sc.nextInt();
+        }
+
+        DFS(answer, 0);
+    }
+
+    public static void DFS(int[] answer, int count) {
+        if(count == M) {
+            print(answer);
+        }
+        else {
+            for(int i=0; i<N; i++) {
+                answer[count] = A[i];
+                DFS(answer, count+1);
+            }
+        }
+    }
+
+    public static void print(int[] answer) {
+        for(int v : answer) {
+            System.out.print(v + " ");
+        }
+        System.out.println();
+    }
+}
