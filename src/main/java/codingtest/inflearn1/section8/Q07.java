db26cba3ecaa3b5511865129e9a6e0f2e67fae2c
hongkikii <hyangmeeh@icloud.com>
2024-03-08 19:48:33 +0900

    [자바 알고리즘 문제풀이 입문] 08-07 base condition 수정 적용

--- src/main/java/codingtest/inflearn1/section8/Q07.java
+++ src/main/java/codingtest/inflearn1/section8/Q07.java
@@ -15,11 +15,9 @@ public class Q07 {
     }
 
     public static int DFS(int n, int r) {
-        if(n == 1) return 1;
-        if(r == 1) return n;
+        if(n == r || r == 0) return 1;
         else {
             return DFS(n-1, r-1) + DFS(n-1, r);
         }
     }
-
 }
