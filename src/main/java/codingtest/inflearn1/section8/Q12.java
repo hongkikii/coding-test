3efaff5d82f9fea49ed96f085ce999f3eb34bfb5
hongkikii <hyangmeeh@icloud.com>
2024-03-09 17:55:28 +0900

    [자바 알고리즘 문제풀이 입문] 08-12 개념 오류 인지 후 수정

--- src/main/java/codingtest/inflearn1/section8/Q12.java
+++ src/main/java/codingtest/inflearn1/section8/Q12.java
@@ -10,68 +10,58 @@ public class Q12 {
         Scanner sc = new Scanner(System.in);
         int M = sc.nextInt();
         int N = sc.nextInt();
-        int[][] box = new int[N+1][M+1];
-        int[][] check = new int[N+1][M+1];
-        int total = 0;
-        int complete = 0;
+        int[][] box = new int[N][M];
+        int[][] check = new int[N][M];
         Queue<Location> queue = new LinkedList<>();
         int[] dx = {-1, 0, 1, 0};
         int[] dy = {0, -1, 0, 1};
-        int result = -1;
-        boolean flag = false;
+        int total = 0;
+        int complete = 0;
 
-        for(int i=1; i<=N; i++) {
-            for(int j=1; j<=M; j++) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
                 box[i][j] = sc.nextInt();
-                if(box[i][j] != -1) {
+                if (box[i][j] != -1) {
                     total++;
-                    if(box[i][j] == 1) {
-                        complete++;
+                    if (box[i][j] == 1) {
                         queue.add(new Location(i, j));
-                        check[i][j] = 1;
+                        complete++;
                     }
-                }
-                else {
-                    check[i][j] = -1;
                 }
             }
         }
 
-        if(total == complete) {
-            System.out.print(0);
+        if (total == complete) {
+            System.out.println(0);
             return;
         }
 
-        while(!queue.isEmpty()){
-            if (flag) {
-                break;
-            }
+        while (!queue.isEmpty()) {
             Location tmp = queue.poll();
-            for(int i=0; i<4; i++) {
+            for (int i = 0; i < 4; i++) {
                 int nx = tmp.row + dx[i];
                 int ny = tmp.column + dy[i];
-                if(nx >= 1 && nx <= N && ny >= 1 && ny <= M) {
-                    while (check[nx][ny] == -1) {
-                        nx += dx[i];
-                        ny += dy[i];
-                        if(!(nx >= 1 && nx <= N && ny >= 1 && ny <= M)) {
-                            break;
-                        }
-                    }
-                    if(nx >= 1 && nx <= N && ny >= 1 && ny <= M && check[nx][ny] == 0) {
-                        queue.add(new Location(nx, ny));
-                        check[nx][ny] = 1;
-                        box[nx][ny] = box[tmp.row][tmp.column] + 1;
-                        complete++;
-                        if(total == complete) {
-                            result = box[nx][ny];
-                            flag = true;
-                        }
-                    }
+                if (nx >= 0 && nx < N && ny >= 0 && ny < M && box[nx][ny] == 0) {
+                    queue.add(new Location(nx, ny));
+                    box[nx][ny] = 1;
+                    check[nx][ny] = check[tmp.row][tmp.column] + 1;
+                    complete++;
                 }
             }
         }
-        System.out.print(result);
+
+        if (total != complete) {
+            System.out.println(-1);
+            return;
+        }
+
+        int result = -9999;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                result = Math.max(result, check[i][j]);
+            }
+        }
+        System.out.println(result);
     }
 
     static class Location {
@@ -82,5 +72,4 @@ public class Q12 {
             this.column = column;
         }
     }
-
 }
