6b8fb3eb23fb3603a22c04fda7c80fd66943ebc6
hongkikii <hyangmeeh@icloud.com>
2024-03-09 15:49:55 +0900

    [자바 알고리즘 문제풀이 입문] 08-11 check 배열 적용

--- src/main/java/codingtest/inflearn1/section8/Q11.java
+++ src/main/java/codingtest/inflearn1/section8/Q11.java
@@ -8,6 +8,7 @@ public class Q11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[][] path = new int[8][8];
+        int[][] check = new int[8][8];
         int[] dx = {-1, 0, 1, 0};
         int[] dy = {0, -1, 0, 1};
         int result = -1;
@@ -20,21 +21,21 @@ public class Q11 {
 
         Queue<Location> queue = new LinkedList<>();
         queue.add(new Location(1, 1));
-        path[1][1] = 1;
+        check[1][1] = 1;
 
         while(!queue.isEmpty()) {
             Location poll = queue.poll();
             if(poll.row == 7 && poll.column==7){
-                result = path[7][7]-1;
+                result = path[7][7];
                 break;
             }
             for(int i=0; i<4; i++) {
                 int x = poll.row + dx[i];
                 int y = poll.column + dy[i];
-                if(x >=1 && x <= 7 && y >=1 && y <= 7 && path[x][y] ==0) {
+                if(x >=1 && x <= 7 && y >=1 && y <= 7 && check[x][y] ==0) {
                     queue.add(new Location(x, y));
-                    path[x][y] = 1;
-                    path[x][y] += path[poll.row][poll.column];
+                    check[x][y] = 1;
+                    path[x][y] = path[poll.row][poll.column] + 1;
                 }
             }
         }
