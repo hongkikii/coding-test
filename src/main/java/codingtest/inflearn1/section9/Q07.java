d236761e03fbd2c98fedb88cc430756f87570637
hongkikii <hyangmeeh@icloud.com>
2024-03-12 16:11:39 +0900

    [자바 알고리즘 문제풀이 입문] 09-07 프림 적용

--- src/main/java/codingtest/inflearn1/section9/Q07.java
+++ src/main/java/codingtest/inflearn1/section9/Q07.java
@@ -1,52 +1,54 @@
 package main.java.codingtest.inflearn1.section9;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Q07 {
-    static int[] arr;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int C = sc.nextInt();
         int R = sc.nextInt();
-        List<Edge> list = new ArrayList<>();
-        arr = new int[C + 1];
-        int result = 0;
-
-        for(int i=1; i<=C; i++) {
-            arr[i] = i;
+        boolean[] check  = new boolean[C+1];
+        List<ArrayList<Edge>> list = new ArrayList<>();
+        for(int i=1; i<=R; i++) {
+            list.add(new ArrayList<>());
         }
-
-        for(int i=0; i<R;  i++) {
+        for(int i=0; i<R; i++) {
             int A = sc.nextInt();
             int B = sc.nextInt();
             int cost = sc.nextInt();
-            list.add(new Edge(A, B, cost));
+            list.get(A).add(new Edge(B, cost));
+            list.get(B).add(new Edge(A, cost));
         }
 
-        Collections.sort(list);
+        Queue<Edge> queue = new PriorityQueue<>();
+        queue.add(new Edge(1, 0));
 
-        for(Edge edge : list) {
-            int A = find(edge.A);
-            int B = find(edge.B);
-            if(A!= B) {
+        int result = 0;
+        while(!queue.isEmpty()) {
+            Edge edge = queue.poll();
+            if(!check[edge.vertex]) {
+                check[edge.vertex] = true;
                 result += edge.cost;
-                arr[A] = B;
+                for(Edge v : list.get(edge.vertex)) {
+                    if(!check[v.vertex]) {
+                        queue.add(v);
+                    }
+                }
             }
         }
         System.out.print(result);
     }
 
     static class Edge implements Comparable<Edge> {
-        int A;
-        int B;
+        int vertex;
         int cost;
 
-        public Edge(int A, int B, int cost) {
-            this.A = A;
-            this.B = B;
+        public Edge(int vertex, int cost) {
+            this.vertex = vertex;
             this.cost = cost;
         }
 
@@ -54,11 +56,6 @@ public class Q07 {
         public int compareTo(Edge o) {
             return this.cost - o.cost;
         }
-    }
-
-    public static int find(int v) {
-        if(arr[v] == v) return v;
-        else return arr[v] = find(arr[v]);
     }
 
 }
