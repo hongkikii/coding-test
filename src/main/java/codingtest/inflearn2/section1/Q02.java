136433c011ffea735e54fef79aba041a4bbe374d
hongkikii <hyangmeeh@icloud.com>
2024-03-14 23:20:07 +0900

    [자바 코딩테스트 - it 대기업 유제] 01-02 간소한 코드 적용

--- src/main/java/codingtest/inflearn2/section1/Q02.java
+++ src/main/java/codingtest/inflearn2/section1/Q02.java
@@ -5,49 +5,22 @@ import java.util.*;
 class Q02 {
     public int[] solution(int[][] board, int k){
         int[] answer = new int[2];
-        int time = 0;
         answer[0] = 0;
         answer[1] = 0;
-        int d = 1; // 1 => 오른쪽, 2 => 아래쪽, 3=> 왼쪽, 4 => 위쪽, …
-        for(int i=0; i<k; i++) {
-            int row = answer[0];
-            int column = answer[1];
-            if(d%4 == 1) {
-                int tmp = column+1;
-                if(tmp >= board.length || board[row][tmp] == 1) {
-                    d++;
-                }
-                else {
-                    answer[1] = tmp;
-                }
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
+        int d = 0;
+        int time = 0;
+        while (time < k) {
+            time++;
+            int nx = answer[0] + dx[d];
+            int ny = answer[1] + dy[d];
+            if (nx < 0 || nx >= board.length || ny < 0 || ny >= board.length || board[nx][ny] == 1) {
+                d = (d + 1) % 4;
+                continue;
             }
-            else if(d%4 == 2) {
-                int tmp = row+1;
-                if(tmp >= board.length || board[tmp][column] == 1) {
-                    d++;
-                }
-                else {
-                    answer[0] = tmp;
-                }
-            }
-            else if(d%4 == 3) {
-                int tmp = column-1;
-                if(tmp < 0 || board[row][tmp] == 1) {
-                    d++;
-                }
-				else {
-                    answer[1] = tmp;
-                }
-            }
-            else if(d%4 == 0) {
-                int tmp = row-1;
-                if(tmp < 0 || board[tmp][column] == 1) {
-                    d++;
-                }
-				else {
-                    answer[0] = tmp;
-                }
-            }
+            answer[0] = nx;
+            answer[1] = ny;
         }
         return answer;
     }
