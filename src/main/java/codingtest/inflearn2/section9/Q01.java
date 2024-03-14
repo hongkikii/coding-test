0e596dd690d534e01b8c804531e4035f5d23dc90
hongkikii <hyangmeeh@icloud.com>
2024-03-14 22:49:39 +0900

    [자바 코딩테스트 - it 대기업 유제] 09-01 간소한 코드 적용

--- src/main/java/codingtest/inflearn2/section9/Q01.java
+++ src/main/java/codingtest/inflearn2/section9/Q01.java
@@ -6,23 +6,19 @@ public class Q01 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int count = sc.nextInt();
-        int money = (int) Math.round(sc.nextDouble() * 100);
-
-        while(!(count ==  0 && money == 0)) {
+        while(true) {
+            int count = sc.nextInt();
+            int money = (int) Math.round(sc.nextDouble() * 100);
+            if(count == 0 && money == 0) break;
             int[] max = new int[money+1];
             for(int i=0; i<count; i++) {
                 int kcal = sc.nextInt();
                 int price = (int) Math.round(sc.nextDouble() * 100);
-
                 for(int j=price; j<=money; j++) {
                     max[j] = Math.max(max[j], max[j-price]+kcal);
                 }
             }
             System.out.println(max[money]);
-            count = sc.nextInt();
-            money = (int) Math.round(sc.nextDouble() * 100);
-
         }
     }
 }
