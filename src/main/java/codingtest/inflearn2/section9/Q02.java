963119041b4f10bd2c17f1c26b367d8d968bd7dd
hongkikii <hyangmeeh@icloud.com>
2024-03-16 22:18:17 +0900

    [자바 코딩테스트 - it 대기업 유제] 09-02 냅색 알고리즘 적용

--- src/main/java/codingtest/inflearn2/section9/Q02.java
+++ src/main/java/codingtest/inflearn2/section9/Q02.java
@@ -1,59 +1,22 @@
 package main.java.codingtest.inflearn2.section9;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
 
 public class Q02 {
-    static List<Pipe> list;
-    static int size;
-    static boolean[] check;
-    static int d;
-    static int result;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        d = sc.nextInt();
-        int count = sc.nextInt();
-        list = new ArrayList<>();
-
-        for(int i=0; i<count; i++) {
-            int length = sc.nextInt();
-            int weight = sc.nextInt();
-            list.add(new Pipe(length, weight));
+        int D = sc.nextInt();
+        int P = sc.nextInt();
+        int[] dy = new int[D+1];
+        for(int i=0; i<P; i++) {
+            int L = sc.nextInt();
+            int C = sc.nextInt();
+            for(int j=D; j>L; j--) {
+                if(dy[j-L] == 0) continue;
+                dy[j] = Math.max(dy[j], Math.min(dy[j-L], C));
+            }
+            dy[L] = Math.max(C, dy[L]);
         }
-        size = list.size();
-        Collections.sort(list);
-        check = new boolean[size];
-        DFS(0, 0, Integer.MAX_VALUE);
-        System.out.println(result);
+        System.out.println(dy[D]);
     }
-
-    public static void DFS(int idx, int length, int min) {
-        if(length == d) {
-            result = Math.max(result, min);
-            return;
-        }
-        if(length > d) return;
-
-        for (int i = idx; i < size; i++) {
-            length += list.get(idx).length;
-            DFS(idx + 1, length, Math.min(min, list.get(i).weight));
-        }
-    }
-
-    static class Pipe implements Comparable<Pipe> {
-        int length;
-        int weight;
-        public Pipe(int length, int weight) {
-            this.length = length;
-            this.weight = weight;
-        }
-
-        @Override
-        public int compareTo(Pipe o) {
-            return this.length - o.length;
-        }
-    }
-
 }
