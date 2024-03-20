fcb6c51fbee4a156d856d8fd288f6231597e2343
hongkikii <hyangmeeh@icloud.com>
2024-03-20 18:30:27 +0900

    [자바 코딩테스트 - it 대기업 유제] 08-03 다익스트라 알고리즘 적용

--- src/main/java/codingtest/inflearn2/section8/Q03.java
+++ src/main/java/codingtest/inflearn2/section8/Q03.java
@@ -3,35 +3,39 @@ package main.java.codingtest.inflearn2.section8;
 import java.util.*;
 
 class Q03 {
-    int[][] min;
-    int m;
-    int n;
     public int solution(int[][] board) {
-        m = board.length;
-        n = board[0].length;
-        min = new int[m][n];
+        int m = board.length;
+        int n = board[0].length;
+        Queue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(a -> a[2]));
+        int[][] cost = new int[m][n];
         for(int i=0; i<m; i++) {
-            for(int j=0; j<n; j++) {
-                min[i][j] = Integer.MAX_VALUE;
+            Arrays.fill(cost[i], Integer.MAX_VALUE);
+        }
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, -1, 0, 1};
+        cost[0][0] = 0;
+        queue.add(new int[]{0, 0, 0}); // 행, 열, 비용
+        while(!queue.isEmpty()) {
+            int[] info = queue.poll();
+            int r = info[0];
+            int c = info[1];
+            int count = info[2];
+            if(count > cost[r][c]) continue;
+            for(int i=0; i<4; i++) {
+                int nx = r + dx[i];
+                int ny = c + dy[i];
+                if(nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
+                if(board[nx][ny] == 0 && cost[nx][ny] > count) {
+                    cost[nx][ny] = count;
+                    queue.add(new int[]{nx, ny, count});
+                }
+                if(board[nx][ny] == 1 && cost[nx][ny] > count+1) {
+                    cost[nx][ny] = count + 1;
+                    queue.add(new int[]{nx, ny, count+1});
+                }
             }
         }
-        min[0][0] = 0;
-        return DP(m-1, n-1, board);
-    }
-
-    public int DP(int row, int column, int[][] board) {
-        if(row == 0 && column == 0) {
-            return min[0][0];
-        }
-        if(row==0) {
-            return min[row][column] = DP(row, column-1, board) + board[row][column];
-        }
-        else if(column ==0) {
-            return min[row][column] = DP(row-1, column, board) + board[row][column];
-        }
-        else {
-            return min[row][column] = Math.min(DP(row-1, column, board), DP(row, column-1, board));
-        }
+        return cost[m-1][n-1];
     }
 
     public static void main(String[] args){
@@ -43,4 +47,5 @@ class Q03 {
         System.out.println(T.solution(new int[][]{{0, 0, 1, 0, 1, 1, 1},{1, 1, 0, 0, 1, 1, 1},{1, 1, 0, 1, 0, 1, 1}, {0, 0, 1, 0, 1, 1, 1}, {1, 0, 1, 0, 1, 1, 0}, {1, 0, 1, 0, 1, 1, 1}, {1, 0, 0, 1, 1, 1, 1}, {1, 1, 0, 0, 1, 1, 1}, {1, 1, 0, 1, 1, 1, 0}}));
     }
 }
+
 
