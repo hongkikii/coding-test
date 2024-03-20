4fe63d9a819bdc207ba2ba722f1f8f1e065648bf
hongkikii <hyangmeeh@icloud.com>
2024-03-20 19:08:57 +0900

    [자바 코딩테스트 - it 대기업 유제] 09-03 DP 적용

---
+++ src/main/java/codingtest/inflearn2/section9/Q03.java
@@ -0,0 +1,31 @@
+package main.java.codingtest.inflearn2.section9;
+
+import java.util.Scanner;
+
+public class Q03 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] A = new int[n+1];
+        int[] score = new int[n+1];
+        for(int i=1; i<=n; i++) {
+            A[i] = sc.nextInt();
+        }
+        if(n==1) {
+            System.out.println(A[1]);
+            return;
+        }
+        if(n==2) {
+            System.out.println(A[1]+A[2]);
+            return;
+        }
+        score[1] = A[1];
+        score[2] = A[1] + A[2];
+        for(int i=3; i<=n; i++) {
+            score[i] = Math.max(score[i-2] + A[i], score[i-3]+A[i-1]+A[i]);
+        }
+        System.out.print(score[n]);
+    }
+
+}
