1e40f83e67713dce7049c57ab1d64475e5a860ec
hongkikii <hyangmeeh@icloud.com>
2024-03-20 17:23:50 +0900

    [자바 코딩테스트 - it 대기업 유제] 07-03 간소한 코드 적용, 미해결
    첫번째 테스트케이스 오답

--- src/main/java/codingtest/inflearn2/section7/Q03.java
+++ src/main/java/codingtest/inflearn2/section7/Q03.java
@@ -4,33 +4,23 @@ import java.util.*;
 class Q03 {
     public int solution(int s, int e){
         int answer = 0;
-        Queue<Point> queue = new LinkedList<>();
+        Queue<Integer> queue = new LinkedList<>();
         boolean[] checkPerson = new boolean[200001];
-        queue.add(new Point(s, e));
+        queue.add(s);
         checkPerson[s] = true;
-        int move = 1;
         while(!queue.isEmpty()) {
             answer++;
             int size = queue.size();
+            e += answer;
+            if(e > 200000) return -1;
             for(int i=0; i<size; i++) {
-                Point point = queue.poll();
-                int nextCow = point.cow + move++;
-                if(nextCow > 200000) return -1;
-                int nextPerson1 = point.person + 1;
-                int nextPerson2 = point.person - 1;
-                int nextPerson3 = point.person * 2;
-                if(nextCow == nextPerson1 || nextCow == nextPerson2 || nextCow == nextPerson3) return answer;
-                if(nextPerson1 >= 0 && nextPerson1 <= 200000 && !checkPerson[nextPerson1]) {
-                    checkPerson[nextPerson1] = true;
-                    queue.add(new Point(nextPerson1, nextCow));
-                }
-                if(nextPerson2 >= 0 && nextPerson2 <= 200000 && !checkPerson[nextPerson2]) {
-                    checkPerson[nextPerson2] = true;
-                    queue.add(new Point(nextPerson2, nextCow));
-                }
-                if(nextPerson3 >= 0 && nextPerson3 <= 200000 && !checkPerson[nextPerson3]) {
-                    checkPerson[nextPerson3] = true;
-                    queue.add(new Point(nextPerson3, nextCow));
+                int cx = queue.poll();
+                for(int nx : new int[]{cx + 1, cx - 1, cx * 2}) {
+                    if(nx == e) return answer;
+                    if(nx >= 0 && nx <= 200000 && !checkPerson[nx]) {
+                        checkPerson[nx] = true;
+                        queue.add(nx);
+                    }
                 }
             }
         }
@@ -44,15 +34,6 @@ class Q03 {
         System.out.println(T.solution(1, 34567));
         System.out.println(T.solution(5, 6));
         System.out.println(T.solution(2, 54321));
-    }
-
-    class Point {
-        int person;
-        int cow;
-        public Point(int person, int cow) {
-            this.person = person;
-            this.cow = cow;
-        }
     }
 }
 
