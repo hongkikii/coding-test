fedb0de099558d46ad9d850693753c060e374776
hongkikii <hyangmeeh@icloud.com>
2024-04-30 21:14:37 +0900

    leetcode_Intervals_56 : 오답 코드

---
+++ src/main/java/codingtest/leetcode/intervals/Q56.java
@@ -0,0 +1,32 @@
+package main.java.codingtest.leetcode.intervals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Q56 {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        for(int i=0; i<intervals.length-1; i++) {
+            if(intervals[i][1] >= intervals[i+1][0]) {
+                intervals[i+1][0] = intervals[i][0];
+                intervals[i][0] = -1;
+                intervals[i][1] = -1;
+            }
+        }
+
+        List<int[]> result = new ArrayList<>();
+        for(int i=0; i<intervals.length; i++) {
+            if(intervals[i][0] != -1) {
+                result.add(intervals[i]);
+            }
+        }
+
+        int[][] returnVal = new int[result.size()][2];
+        for(int i=0; i<returnVal.length; i++) {
+            returnVal[i] = result.get(i);
+        }
+
+        return returnVal;
+    }
+}
