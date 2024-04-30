11464adf4cb101809d4444b6697c915c945d7645
hongkikii <hyangmeeh@icloud.com>
2024-04-30 21:15:08 +0900

    leetcode_Intervals_56 : 정답 코드

--- src/main/java/codingtest/leetcode/intervals/Q56.java
+++ src/main/java/codingtest/leetcode/intervals/Q56.java
@@ -10,6 +10,7 @@ public class Q56 {
         for(int i=0; i<intervals.length-1; i++) {
             if(intervals[i][1] >= intervals[i+1][0]) {
                 intervals[i+1][0] = intervals[i][0];
+                intervals[i+1][1] = Math.max(intervals[i][1], intervals[i+1][1]);
                 intervals[i][0] = -1;
                 intervals[i][1] = -1;
             }
