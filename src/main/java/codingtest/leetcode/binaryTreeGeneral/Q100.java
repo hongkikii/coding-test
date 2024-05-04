8045e911ba5cdaa474e073cfe987380dbb806a34
hongkikii <hyangmeeh@icloud.com>
2024-05-05 00:04:25 +0900

    leetcode_BinaryTreeGeneral_100 : return에 구조 확인 조건 적용

--- src/main/java/codingtest/leetcode/binaryTreeGeneral/Q100.java
+++ src/main/java/codingtest/leetcode/binaryTreeGeneral/Q100.java
@@ -3,14 +3,11 @@ package main.java.codingtest.leetcode.binaryTreeGeneral;
 public class Q100 {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p == null && q == null) return true;
-        if(p == null) return false;
-        if(q == null) return false;
-        if(p.val != q.val) return false;
-        else {
-            isSameTree(p.left, q.left);
-            isSameTree(p.right, q.right);
-            return true;
+        if(p == null || q == null) return false;
+        if(p.val != q.val) {
+            return false;
         }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
     static class TreeNode {
         int val;
