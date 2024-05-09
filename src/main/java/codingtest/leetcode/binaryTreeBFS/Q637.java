f233a64df8f3f54dafa5616b6446996944846d7e
hongkikii <hyangmeeh@icloud.com>
2024-05-09 17:58:22 +0900

    leetcode_BinaryTreeBFS_637 : 정답 코드

---
+++ src/main/java/codingtest/leetcode/binaryTreeBFS/Q637.java
@@ -0,0 +1,41 @@
+package main.java.codingtest.leetcode.binaryTreeBFS;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class Q637 {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        double sum = 0.0;
+
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            for(int i=0; i<size; i++) {
+                TreeNode node = queue.poll();
+                sum += node.val;
+                if(node.left != null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
+            }
+            result.add(sum/size);
+            sum = 0;
+        }
+        return result;
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
