d203f78f1e185b30aab5f8e008926f16bd8f975a
hongkikii <hyangmeeh@icloud.com>
2024-05-13 23:07:07 +0900

    leetcode_BitManipulation_67 : 오답 코드
    이전보다 조금 더 많은 테스트 케이스 커버, 아무래도 데이터 타입의 한계가 있으니 비트연산으로 가야 할 듯

--- src/main/java/codingtest/leetcode/bitManipulation/Q67.java
+++ src/main/java/codingtest/leetcode/bitManipulation/Q67.java
@@ -4,15 +4,15 @@ public class Q67 {
     public String addBinary(String a, String b) {
         char[] aChars = a.toCharArray();
         char[] bChars = b.toCharArray();
-        int aInt = toInt(aChars);
-        int bInt = toInt(bChars);
-        int sum = aInt + bInt;
+        long aNum = toNumber(aChars);
+        long bNum = toNumber(bChars);
+        long sum = aNum + bNum;
         return toBinaryString(sum);
     }
 
-    private int toInt(char[] binaryCharArray) {
-        int digit = 1;
-        int result = 0;
+    private long toNumber(char[] binaryCharArray) {
+        long digit = 1;
+        long result = 0;
         for(int i=binaryCharArray.length-1; i>=0; i--) {
             result += (binaryCharArray[i] - 48) * digit;
             digit *= 2;
@@ -20,7 +20,7 @@ public class Q67 {
         return result;
     }
 
-    private String toBinaryString(int num) {
+    private String toBinaryString(long num) {
         if(num == 0) return String.valueOf(0);
         StringBuilder sb = new StringBuilder();
         while(num > 0) {
