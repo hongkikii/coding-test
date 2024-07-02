94320917a49ff83f175d60301d018601b31c7d6a
hongkikii <hyangmeeh@icloud.com>
2024-07-02 16:47:21 +0900

    캠프 : 5-2 오답 코드

---
+++ src/main/java/codingtest/camp/Q5_2.java
@@ -0,0 +1,33 @@
+package main.java.codingtest.camp;
+
+import java.util.*;
+
+public class Q5_2 {
+    public int[] solution(int[] progresses, int[] speeds) {
+        List<Integer> answer = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int i=0; i<progresses.length; i++) {
+            int rest = 100-progresses[i];
+            int day = rest/speeds[i];
+            if(rest % speeds[i] > 0) day++;
+            queue.offer(day);
+        }
+
+        while(!queue.isEmpty()) {
+            int currentDay = queue.poll();
+            int value = 1;
+            if(queue.isEmpty()) {
+                answer.add(value);
+                break;
+            }
+            while(!queue.isEmpty() && queue.peek() < currentDay) {
+                queue.poll();
+                value++;
+            }
+            answer.add(value);
+        }
+
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
