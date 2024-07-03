2b5706402dd257d5729f2cde8010c99d3214b5ee
hongkikii <hyangmeeh@icloud.com>
2024-07-03 13:34:13 +0900

    캠프 : 6-1 정답 코드

--- src/main/java/codingtest/camp/Q6_1.java
+++ src/main/java/codingtest/camp/Q6_1.java
@@ -1,13 +1,12 @@
 package main.java.codingtest.camp;
 
+import java.util.*;
+
 public class Q6_1 {
     public boolean solution(String[] phone_book) {
-        for(int i=0; i<phone_book.length; i++) {
-            for(int j=i+1; j<phone_book.length; j++) {
-                if(i == j) continue;
-                if(phone_book[j].startsWith(phone_book[i])) return false;
-                if(phone_book[i].startsWith(phone_book[j])) return false;
-            }
+        Arrays.sort(phone_book);
+        for(int i=0; i<phone_book.length-1; i++) {
+            if(phone_book[i+1].startsWith(phone_book[i])) return false;
         }
         return true;
     }
