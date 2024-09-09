fe57ec436c250a9dc832fc6896ab347179f87b3b
hongkikii <hyangmeeh@icloud.com>
2024-09-09 19:01:17 +0900

    leetcode_Stack_71 : 정답 코드

--- src/main/java/codingtest/leetcode/stack/Q71.java
+++ src/main/java/codingtest/leetcode/stack/Q71.java
@@ -7,7 +7,7 @@ public class Q71 {
 
     public String simplifyPath(String path) {
         // . -> 아무 일 없음
-        // .. -> 스택에서 하나 제거
+        // .. -> 스택에서 하나 제거, 루트인 경우 상위로 못 감
         // 슬래시만 여러개 -> 스택에 추가(슬래시 하나로 처리)
         // 나머지 -> 스택에 추가(유효한 디렉터리 이름)
         // 마지막 슬래시 -> 아무 일 없음(결과에 포함 X)
@@ -16,11 +16,15 @@ public class Q71 {
         // 슬래시는 나중에 별도로 추가해서 조건을 간단히 하자
 
         Deque<String> deque = new LinkedList<>();
-        String[] splits = path.split("/+");
+//        String[] splits = path.split("/+");
+        // 슬래시로 시작하는 문자열 있을 시, 그 앞 에 빈 문자열이 있다고 간주
+        String[] splits = path.replaceFirst("^/+", "").split("/+");
 
         for(String split : splits) {
             if(split.equals(".")) continue;
-            if(split.equals("..") && deque.size() > 1)  deque.removeLast();
+            if(split.equals("..")) {
+                if(deque.size() >= 1) deque.removeLast();
+            }
             else deque.addLast(split);
         }
 
@@ -29,13 +33,13 @@ public class Q71 {
         for(String str : deque) {
             sb.append(str + "/");
         }
-        sb.deleteCharAt(sb.length()-1);
+        if(sb.length() > 1) sb.deleteCharAt(sb.length()-1);
 
         return sb.toString();
     }
 
     public static void main(String[] args) {
         Q71 q71 = new Q71();
-        System.out.println(q71.simplifyPath("/home/"));
+        System.out.println(q71.simplifyPath("/../"));
     }
 }
