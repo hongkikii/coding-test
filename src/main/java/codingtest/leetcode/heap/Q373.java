75ce3b5ccc836b72265265a54e448c38d390dd9a
hongkikii <hyangmeeh@icloud.com>
2024-09-10 17:51:25 +0900

    leetcode_Heap_373 : 최소 힙 적용

--- src/main/java/codingtest/leetcode/heap/Q373.java
+++ src/main/java/codingtest/leetcode/heap/Q373.java
@@ -9,17 +9,19 @@ public class Q373 {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
         List<List<Integer>> answer = new ArrayList<>();
         Queue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0] + a[1] - b[0] - b[1]);
-        for(int i=0; i<nums1.length; i++) {
-            for(int j=0; j<nums2.length; j++) {
-                minHeap.add(new int[]{nums1[i], nums2[j]});
-            }
+        for(int i=0; i<Math.min(nums1.length, k); i++) {
+            minHeap.add(new int[]{nums1[i], nums2[0], 0});
         }
         while(k > 0) {
-            List<Integer> pair = new ArrayList<>();
             int[] min = minHeap.poll();
+            List<Integer> pair = new ArrayList<>();
             pair.add(min[0]);
             pair.add(min[1]);
             answer.add(pair);
+
+            if(min[2] + 1 < nums2.length) {
+                minHeap.offer(new int[]{min[0], nums2[min[2] + 1], min[2] + 1});
+            }
             k--;
         }
         return answer;
